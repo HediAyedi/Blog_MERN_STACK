3b117cf904d035baeff9e035b33504836dcc6984
Hedi Ayedi <hediayedi20@gmail.com>
2021-02-05 08:37:11 +0100

    converting html to react

---
+++ src/components/screens/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../parts/Header';
+import Footer from '../parts/Footer';
+import FreshStories from '../parts/FreshStories';
+
+
+export function Home () {
+    return <>
+        <Header />
+        Hello World
+        <Footer/>
+    </>
+}
